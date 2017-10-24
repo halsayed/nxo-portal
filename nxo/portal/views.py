3d2acd5409550a977acd3415c31964df39601768
Husain Ebrahim <hus.alsayed@gmail.com>
2017-10-24 05:11:08 +0300

    added 'templates' dir under 'portal' added templates path to settings.py created 'uploads' dir created initial upload.py added upload to views.py added '/upload/' to views.py

--- nxo/portal/views.py
+++ nxo/portal/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .upload import UploadFileForm
+
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the portal index.")
+
+
+def upload(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # handle_uploaded_file(request.FILES['file'])
+            return HttpResponse('file uploaded')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
